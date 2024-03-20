1d5e2d0365c252e71e1483e91fcb629d95994774
Lehigh HPC <hpc@lehigh.edu>
2024-03-20 10:12:22 -0400

    modulefiles point to scratch

--- share/spack/templates/modules/modulefile.lua
+++ share/spack/templates/modules/modulefile.lua
@@ -98,4 +98,14 @@ append_path("MANPATH", "", ":")
 
 {% block footer %}
 {# In case the module needs to be extended with custom Lua code #}
+local cephscratch = pathJoin("/share/ceph/scratch/",os.getenv('USER'),"/",os.getenv('SLURM_JOB_ID')) or pathJoin(os.getenv('HOME'),"/JOB_TMPDIR")
+local tmpscratch = pathJoin("/scratch/",os.getenv('USER'),"/",os.getenv('SLURM_JOB_ID')) or pathJoin(os.getenv('HOME'),"/JOB_TMPDIR")
+--- Setup CEPH SCRATCH and LOCAL SCRATCH                                        
+setenv("LOCAL_SCRATCH",tmpscratch)                                              
+setenv("CEPHFS_SCRATCH",cephscratch)                                            
+if os.getenv('SLURM_JOB_ID') == nil then                                        
+  setenv("TMPDIR",pathJoin(os.getenv('HOME'),"/JOB_TMPDIR"))                    
+else                                                                            
+  setenv("TMPDIR",cephscratch)                                                  
+end  
 {% endblock %}
