f4ebe67bbe7d5a265061f96a62fd75ab594bc375
Lehigh HPC <hpc@lehigh.edu>
2025-10-08 16:04:45 -0400

    critical updates for zen25v1

--- var/spack/repos/builtin/packages/orca/package.py
+++ var/spack/repos/builtin/packages/orca/package.py
@@ -23,6 +23,9 @@ class Orca(Package):
 
     license("LGPL-2.1-or-later")
 
+    # rpb222 adds this
+    version("6.1.0", sha256="3df2d25c36fe7e49f1c301b3ad6cfc9b837a370fd3cee31275a86ecd291ca944")
+
     version(
         "avx2-6.0.0", sha256="02c21294efe7b1b721e26cb90f98ee15ad682d02807201b7d217dfe67905a2fd"
     )
